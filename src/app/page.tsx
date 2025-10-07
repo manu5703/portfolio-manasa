253c47885437015959beaa0a4ca5f4ccae0d1dab
manu5703 <109054430+manu5703@users.noreply.github.com>
2025-10-06 20:37:05 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import About from "@/components/About";
 import Carousel from "@/components/Carousel";
-import Hello from "@/components/Hello";
+// import Hello from "@/components/Hello";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
